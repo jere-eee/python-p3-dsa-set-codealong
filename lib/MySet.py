2a34bed64d4389a0fff608bf0f7474b1debb615f
Akanle Jeremy <akanlejeremy@gmail.com>
2024-12-11 14:49:10 +0300

    O(1) runtime exercises

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,20 @@
 class MySet:
 
-    pass
+    def __init__(self, enumerable=[]):
+        self.dictionary = {}
+        for val in enumerable:
+            self.dictionary[val] = True
+    
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
